028561dff1ca7ceb2660e01d213b5f6f7aebcda0
dalibor <dlipovac012@gmail.com>
2020-04-20 11:15:15 +0200

    Removed unnecessary code

--- stack_example.c
+++ stack_example.c
@@ -1,21 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  stack_example.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  04/19/2020 07:47:41 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
- *        Company:  FH Südwestfalen, Iserlohn
- *
- * =====================================================================================
- */
-
 void test_function(int a, int b, int c, int d)
 {
 	int flag;
