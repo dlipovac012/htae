a01069ad324f3b7ec108100bc071ffe18933c7bb
dalibor <dlipovac012@gmail.com>
2020-04-20 10:18:36 +0200

    Some additions

---
+++ stack_example.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  stack_example.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/19/2020 07:47:41 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+ *        Company:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+
+void test_function(int a, int b, int c, int d)
+{
+	int flag;
+	char buffer[10];
+
+	flag = 31337;
+	buffer[0] = 'A';
+}
+
+int main()
+{
+	test_function(1, 2, 3, 4);
+
+	return 0;
+}
