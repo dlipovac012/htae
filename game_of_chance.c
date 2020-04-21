710f3827af52f362662309f2fcd8808358be2e0a
dalibor <dalibor>
2020-04-21 17:20:50 +0200

    continuation

--- game_of_chance.c
+++ game_of_chance.c
@@ -80,18 +80,83 @@ int main(int argc, char **argv)
 			printf("\n[!!] The number %d is an invalid selection. Choose wisely. \n\n", choice);
 		else if (choice < 4)
 		{
-			
+			if (choice != last_game)
+			{
+				if (choice == 1)
+					player.current_game = pick_a_number;
+				else if (choice == 2)
+					player.current_game = dealer_no_match;
+				else
+					player.current_game = find_the_ace;
+				last_game = choice;
+			}
+			play_the_game();
+		}
+		else if (choice == 4)
+			show_highscore();
+		else if (choice == 5)
+		{
+			printf("\nChange user name\n");
+			printf("Enter your new name: ");
+			input_name();
+			printf("Your name has been changed.\n\n");
+		}
+		else if (choice == 6)
+		{
+			printf("\nYour account has been reset with 100 credits.\n\n");
+			player.credits = 100;
 		}
 	}
-
-
+	update_player_data();
+	printf("\nThanks for playing! Bye bye.\n");
 
 	return 0;
 }
 
+int get_player_data()
+{
+	int fd, uid, read_bytes;
+	User entry;
 
+	uid = getuid();
 
+	fd = open(DATAFILE, O_RDONLY);
+	if (fd == -1)
+		return -1;
+	read_bytes = read(fd, &entry, sizeof(User));
+	while(entry.uid != uid && read_bytes > 0)
+	{
+		read_bytes = read(fd, &entry, sizeof(User));
+	}
+	close(fd);
+	
+	if (ready_bytes < sizeof(User))
+		return -1;
+	else
+		player = entry;
+	return 1;
+}
 
+void register_new_player()
+{
+	int fd;
+
+	printf("-=-={ New Player Registration }=-=-\n");
+	printf("Enter your name: ");
+	input_name();
+
+	player.uid = getuid();
+	player.highscore = player.credits = 100;
+
+	fd = open(DATAFILE, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+	if (fd == -1)
+		fatal("in register_new_player() while opening file");
+	write(fd, &player, sizeof(User));
+	close(fd);
+
+	printf("\nWelcome to the Game of Chance %s.\n", player.name);
+	printf("You have been given %u credits.\n", player.credits);
+}
 
 
 
